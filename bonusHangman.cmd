f64ca09da57ace979b9ed4b93e5fb142c30b229d
vansh barot <vansh.jayeshbarot@gmail.com>
2025-12-04 14:45:53 -0500

    Add initial script skeleton

---
+++ bonusHangman.cmd
@@ -0,0 +1,11 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+rem Hangman Script - Stage 1
+rem Basic skeleton with no game logic yet.
+
+echo Hangman Script Starting...
+echo.
+echo This is the basic skeleton.
+echo More features will be added in later commits.
+pause
