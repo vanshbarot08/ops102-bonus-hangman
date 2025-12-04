e0b80a4c53509e61d5fb29bae849ec903da4b6d4
vansh barot <vansh.jayeshbarot@gmail.com>
2025-12-04 17:57:46 -0500

    Add main menu navigation system

--- bonusHangman.cmd
+++ bonusHangman.cmd
@@ -1,11 +1,35 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-rem Hangman Script - Stage 1
-rem Basic skeleton with no game logic yet.
+rem Hangman Script - Stage 2
+rem Menu structure added
 
-echo Hangman Script Starting...
+:MENU
+cls
+echo =========================
+echo          HANGMAN
+echo =========================
+echo 1. Play Game
+echo 2. Scoreboard
+echo 3. Quit
 echo.
-echo This is the basic skeleton.
-echo More features will be added in later commits.
+set /p CHOICE=Enter choice: 
+
+if "%CHOICE%"=="1" goto PLAY
+if "%CHOICE%"=="2" goto SCORE
+if "%CHOICE%"=="3" goto END
+goto MENU
+
+:PLAY
+echo Play selected.
 pause
+goto MENU
+
+:SCORE
+echo Scoreboard selected.
+pause
+goto MENU
+
+:END
+echo Goodbye!
+exit /b
